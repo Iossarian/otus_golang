7b34d0f68b4744dc8e51a73f04df078a8284fcd0
KleymenovN <kleymenov68@yandex.ru>
2022-03-20 12:13:00 +0300

    hw03 have been done

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -6,17 +6,16 @@ import (
 )
 
 func Top10(text string) []string {
+	if len(text) == 0 {
+		return make([]string, 0)
+	}
 	explodedSlice := strings.Fields(text)
 	dictionaryMap := make(map[string]int)
 	for _, word := range explodedSlice {
-		if dictionaryMap[word] == 0 {
-			dictionaryMap[word] = 1
-		} else {
-			dictionaryMap[word]++
-		}
+		dictionaryMap[word]++
 	}
-	dictionaries := sortByRepetitions(dictionaryMap)
-	return sortLexicographically(dictionaries)
+
+	return sortByRepetitions(dictionaryMap)
 }
 
 type dictionary struct {
@@ -24,34 +23,30 @@ type dictionary struct {
 	RepeatCount int
 }
 
-func sortLexicographically(dictionaries []dictionary) []string {
-	groupedByRepeatsCountDict := make(map[int][]string)
-	for _, v := range dictionaries {
-		groupedByRepeatsCountDict[v.RepeatCount] = append(groupedByRepeatsCountDict[v.RepeatCount], v.Word)
-	}
-	for _, words := range groupedByRepeatsCountDict {
-		sort.SliceStable(words, func(i, j int) bool {
-			return words[i] < words[j]
-		})
-	}
-	result := make([]string, 0, 10)
-	for i := len(groupedByRepeatsCountDict) + 1; i > 0; i-- {
-		if len(result) < 10 {
-			result = append(result, groupedByRepeatsCountDict[i]...)
-		}
-	}
-
-	return result
-}
-
-func sortByRepetitions(dictionaryMap map[string]int) []dictionary {
+func sortByRepetitions(dictionaryMap map[string]int) []string {
 	dictionaries := make([]dictionary, 0, len(dictionaryMap))
 	for k, v := range dictionaryMap {
 		dictionaries = append(dictionaries, dictionary{k, v})
 	}
-	sort.Slice(dictionaries, func(i, j int) bool {
+	sort.SliceStable(dictionaries, func(i, j int) bool {
+		if dictionaries[i].RepeatCount == dictionaries[j].RepeatCount {
+			return dictionaries[i].Word < dictionaries[j].Word
+		}
 		return dictionaries[i].RepeatCount > dictionaries[j].RepeatCount
 	})
 
-	return dictionaries
+	return fillResult(dictionaries)
+}
+
+func fillResult(dictionaries []dictionary) []string {
+	result := make([]string, 0, 10)
+	dictionaryLen := len(dictionaries)
+	for i := 0; i < 10; i++ {
+		if i >= dictionaryLen {
+			break
+		}
+		result = append(result, dictionaries[i].Word)
+	}
+
+	return result
 }
