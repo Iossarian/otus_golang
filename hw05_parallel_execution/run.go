dc65c57702f62799bc70ae0684550b29bf481179
KleymenovN <kleymenov68@yandex.ru>
2022-04-19 21:36:14 +0300

    hw 05 is ready

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -2,6 +2,7 @@ package hw05parallelexecution
 
 import (
 	"errors"
+	"sync"
 )
 
 var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
@@ -10,6 +11,32 @@ type Task func() error
 
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, n, m int) error {
-	// Place your code here.
+	wg := sync.WaitGroup{}
+	ch := make(chan Task)
+	errCount := 0
+	mutex := sync.Mutex{}
+	for i := 1; i <= n; i++ {
+		go func(ci chan Task) {
+			defer wg.Done()
+			wg.Add(1)
+			for task := range ci {
+				taskError := task()
+				if taskError != nil && errCount < m {
+					mutex.Lock()
+					errCount++
+					mutex.Unlock()
+				}
+			}
+		}(ch)
+	}
+	for _, t := range tasks {
+		if errCount == m {
+			close(ch)
+			return ErrErrorsLimitExceeded
+		}
+		ch <- t
+	}
+	close(ch)
+	wg.Wait()
 	return nil
 }
--- hw05_parallel_execution/run_test.go
+++ hw05_parallel_execution/run_test.go
@@ -38,6 +38,27 @@ func TestRun(t *testing.T) {
 		require.LessOrEqual(t, runTasksCount, int32(workersCount+maxErrorsCount), "extra tasks were started")
 	})
 
+	t.Run("tasks with only errors", func(t *testing.T) {
+		tasksCount := 50
+		tasks := make([]Task, 0, tasksCount)
+
+		var runTasksCount int32
+
+		for i := 0; i < tasksCount; i++ {
+			err := fmt.Errorf("error from task %d", i)
+			tasks = append(tasks, func() error {
+				time.Sleep(time.Millisecond * time.Duration(rand.Intn(100)))
+				atomic.AddInt32(&runTasksCount, 1)
+				return err
+			})
+		}
+
+		workersCount := 10
+		maxErrorsCount := 0
+		err := Run(tasks, workersCount, maxErrorsCount)
+		require.Truef(t, errors.Is(err, ErrErrorsLimitExceeded), "actual err - %v", err)
+	})
+
 	t.Run("tasks without errors", func(t *testing.T) {
 		tasksCount := 50
 		tasks := make([]Task, 0, tasksCount)
@@ -66,5 +87,33 @@ func TestRun(t *testing.T) {
 
 		require.Equal(t, runTasksCount, int32(tasksCount), "not all tasks were completed")
 		require.LessOrEqual(t, int64(elapsedTime), int64(sumTime/2), "tasks were run sequentially?")
+	})
+
+	t.Run("tasks without errors 2", func(t *testing.T) {
+		tasksCount := 50
+		tasks := make([]Task, 0, tasksCount)
+
+		var runTasksCount int32
+		var sumTime time.Duration
+
+		for i := 0; i < tasksCount; i++ {
+			tasks = append(tasks, func() error {
+				taskSleep := time.Millisecond * time.Duration(rand.Intn(100))
+				sumTime += taskSleep
+				atomic.AddInt32(&runTasksCount, 1)
+				return nil
+			})
+		}
+
+		workersCount := 5
+		maxErrorsCount := 1
+		start := time.Now()
+		Run(tasks, workersCount, maxErrorsCount)
+		elapsedTime := time.Since(start)
+
+		require.Equal(t, runTasksCount, int32(tasksCount), "not all tasks were completed")
+		require.Eventually(t, func() bool {
+			return int64(elapsedTime) <= int64(sumTime/2)
+		}, time.Second, time.Millisecond)
 	})
 }
